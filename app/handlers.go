445a21148247e4198bdf4398bdb8751c5a217af2
tambarie <gt.gbaragbo@gmail.com>
2022-02-18 03:41:10 +0100

    add getAllCustomers handlers

--- app/handlers.go
+++ app/handlers.go
@@ -1,1 +1,28 @@
 package app
+
+import (
+	"bank-application/service"
+	"encoding/json"
+	"encoding/xml"
+	"net/http"
+)
+
+type CustomerHandlers struct {
+	service service.CustomerService
+}
+
+func (c *CustomerHandlers) getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers, err := c.service.GetAllCustomers()
+	if err != nil {
+		panic(err)
+	}
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+
+}
