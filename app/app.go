cd59af06bc4b33dda54d2327d66c720750e3c7b6
tambarie <gt.gbaragbo@gmail.com>
2022-02-18 03:39:46 +0100

    initiated the server

--- app/app.go
+++ app/app.go
@@ -1,1 +1,24 @@
 package app
+
+import (
+	"bank-application/domain"
+	"bank-application/service"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func Start() {
+	router := mux.NewRouter()
+
+	//wiring
+	c := CustomerHandlers{service: service.NewCustomerService(domain.NewCustomerRepositoryStub())}
+	// define routes
+	router.HandleFunc("/customers", c.getAllCustomers).Methods(http.MethodGet)
+
+	fmt.Println("server started successfully")
+	//starting server
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+
+}
