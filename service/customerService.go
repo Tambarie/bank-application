2bc566de0a10075b08103d7c2aad271b9121d4ed
tambarie <gt.gbaragbo@gmail.com>
2022-02-18 03:40:39 +0100

    add GetAllCustomers method

--- service/customerService.go
+++ service/customerService.go
@@ -6,12 +6,16 @@ type CustomerService interface {
 	GetAllCustomers() ([]domain.Customer, error)
 }
 
-type DefaultCustomerSrvice struct {
+type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerSrvice {
-	return DefaultCustomerSrvice{
+func (s DefaultCustomerService) GetAllCustomers() ([]domain.Customer, error) {
+	return s.repo.FindAll()
+}
+
+func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
+	return DefaultCustomerService{
 		repo: repository,
 	}
 }
